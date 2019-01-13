0f58ac66b09cf9156db38f148b35555941a8d1d1
hkab <khoahockithuatmc@gmail.com>
2019-01-13 22:25:03 +0700

    cookie Jar to dict

--- utils.py
+++ utils.py
@@ -16,6 +16,5 @@ def getCookiesInUS():
 	payload = {"locationType": "LOCATION_INPUT", "zipCode": 10010, "storeContext": "generic", "deviceType":"web", "pageType":"Gateway", "actionSource":"glow"}
 	headers = { "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", "accept": "text/html,*/*"}
 	r = requests.post("https://www.amazon.com/gp/delivery/ajax/address-change.html", data=payload, headers=headers)
-
-
-	return r.cookies
+	
+	return requests.utils.dict_from_cookiejar(r.cookies)
