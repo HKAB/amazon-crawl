ecdeafe1a55c5b2b30568d78dc95604d8a1f83f3
hkab <khoahockithuatmc@gmail.com>
2019-01-13 22:12:24 +0700

    stupid sytax error

--- utils.py
+++ utils.py
@@ -8,11 +8,12 @@ def readFile(file):
 	return data.split('\n')
 
 def notif(n):
-	print("[INFO] "n)
+	print("[INFO] " + n)
 
 def getCookiesInUS():
 	payload = {"locationType": "LOCATION_INPUT", "zipCode": 10010, "storeContext": "generic", "deviceType":"web", "pageType":"Gateway", "actionSource":"glow"}
 	headers = { "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", "accept": "text/html,*/*"}
 	r = requests.post("https://www.amazon.com/gp/delivery/ajax/address-change.html", data=payload, headers=headers)
+
 
 	return r.cookies
