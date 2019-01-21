ce3d101078d67b1729aa3f479a3ca0a82ea6b5a8
HKAB <khoahockithuatmc@gmail.com>
2019-01-21 08:21:04 +0700

    window use \r\n for a newline, but i'm coding in unix :(, so change the readfile function

--- utils.py
+++ utils.py
@@ -6,9 +6,9 @@ def removeSpaceAndStrip(s):
 	return s.replace("\t", "").replace("\n", "").strip()
 
 def readFile(file):
-	f = open(file, 'r')
+	f = open(file, 'rb')
 	data = f.read()
-	return data.split('\n')
+	return data.split('\r\n')
 
 def notif(n):
 	print("[ INFO] " + n)
@@ -17,7 +17,7 @@ def getCookiesInUS():
 	payload = {"locationType": "LOCATION_INPUT", "zipCode": 10010, "storeContext": "generic", "deviceType":"web", "pageType":"Gateway", "actionSource":"glow"}
 	headers = { "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36", "accept": "text/html,*/*"}
 	r = requests.post("https://www.amazon.com/gp/delivery/ajax/address-change.html", data=payload, headers=headers)
-	
+
 	return requests.utils.dict_from_cookiejar(r.cookies)
 def isASCIIString(string):
 	try:
